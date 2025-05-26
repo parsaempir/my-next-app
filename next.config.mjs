4df7bb3bbd67c310b95971970a29574cbfff5ec5
parsaempir <parsa.aysham54@gmail.com>
2025-05-26 17:42:04 +0330

    ./

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
